c27446c1bf73514baef20c2bba9530d527c07e33
emiz98 <64089619+emiz98@users.noreply.github.com>
2022-02-24 11:06:02 +0530

    Build Fix #1

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import Header from "./components/Header";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Main from "./components/Main";
+import response from "./response";
 
 function App() {
   const [punkListData, setPunkListData] = useState([]);
+  const developmentServer = false;
 
   useEffect(() => {
-    const getMyNfts = async () => {
-      const openseaData = await axios.get(
-        "https://testnets-api.opensea.io/assets?asset_contract_address=0xE3701F48A27f5868886fE6b04f914dD8D213294a&order_direction=desc"
-      );
-      setPunkListData(openseaData.data.assets);
-    };
-    getMyNfts();
+    if (developmentServer) {
+      const getMyNfts = async () => {
+        const openseaData = await axios.get(
+          "https://testnets-api.opensea.io/assets?asset_contract_address=0xE3701F48A27f5868886fE6b04f914dD8D213294a&order_direction=desc"
+        );
+        setPunkListData(openseaData.data.assets);
+      };
+      getMyNfts();
+    } else {
+      setPunkListData(response.items);
+    }
   }, []);
 
   return (
